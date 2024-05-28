77baa5deecfe71a69c64efbef506fd900abe25cc
masudrana-1 <masudrana58855@gmail.com>
2024-05-28 09:41:02 +0600

    create logo and responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Masud Rana E-Commerce Application",
+  title: "Wwwshops E-Commerce Application",
   description: "A complete e-commerce application with Next.js and Wix",
 };
 
