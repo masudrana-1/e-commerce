f451d0df5d99be95ecf4d9b6cc2106ce399b4b91
masudrana-1 <masudrana58855@gmail.com>
2024-05-28 10:44:15 +0600

    create big screen nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,25 @@ import Menu from './Menu';
 const Navbar = () => {
     return (
         <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
-            <div className='h-full flex items-center justify-between'>
                 {/* mobile  */}
+            <div className='h-full flex items-center justify-between'>
                 <Link href="/">
                     <div className='text-2xl tracking-wide'>Wwwshops</div>
                 </Link>
                 <Menu/>
+            </div>
+
+            {/* big screen  */}
+            <div className='hidden md:flex items-center justify-between gap-8 h-full'>
+                {/* left side  */}
+                <div>
+                    
+                </div>
+
+                {/* right side  */}
+                <div>
+
+                </div>
             </div>
         </div>
     );
