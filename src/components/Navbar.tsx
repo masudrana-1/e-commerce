3874a4445f0f1bc54c9a059dca179849bea47347
masudrana-1 <masudrana58855@gmail.com>
2024-05-28 09:53:56 +0600

    create mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
+import Menu from './Menu';
 
 const Navbar = () => {
     return (
-        <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
-            {/* mobile  */}
-            <Link href="/">Wwwshops</Link>
+        <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+            <div className='h-full flex items-center justify-between'>
+                {/* mobile  */}
+                <Link href="/">
+                    <div className='text-2xl tracking-wide'>Wwwshops</div>
+                </Link>
+                <Menu/>
+            </div>
         </div>
     );
 };
