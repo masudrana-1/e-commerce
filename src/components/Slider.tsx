27e53a6c529b9c0baff8908501bd831f39077132
masudrana-1 <masudrana58855@gmail.com>
2024-05-30 20:02:38 +0600

    create slider indicator

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -56,7 +56,7 @@ const Slider = () => {
                             </h1>
                             <Link href={slide.url}>
                                 <button className="rounded-md bg-black text-white py-3 px-4 ">
-                                SHOP NOW
+                                    SHOP NOW
                                 </button>
                             </Link>
                         </div>
@@ -70,6 +70,21 @@ const Slider = () => {
                                 className="object-cover"
                             />
                         </div>
+                    </div>
+                ))}
+            </div>
+            <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+                {slides.map((slide, index) => (
+                    <div
+                        className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+                        current === index ? "scale-150" : ""
+                        }`}
+                        key={slide.id}
+                        onClick={() => setCurrent(index)}
+                    >
+                        {current === index && (
+                            <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+                        )}
                     </div>
                 ))}
             </div>
