80b84ce0df7ac3fe9eda83c22b2a3ac4d45077c2
masudrana-1 <masudrana58855@gmail.com>
2024-05-30 19:57:30 +0600

    added

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const slides = [
@@ -34,8 +36,43 @@ const Slider = () => {
     const [current, setCurrent] = useState(0);
 
     return (
-        <div>
-            
+        <div className="h-[calc(100vh-80px)] overflow-hidden">
+            <div
+                className="w-max h-full flex transition-all ease-in-out duration-1000"
+                style={{ transform: `translateX(-${current * 100}vw)` }}
+            >
+                {slides.map((slide) => (
+                    <div
+                        className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+                        key={slide.id}
+                    >
+                        {/* TEXT CONTAINER */}
+                        <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+                            <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                                {slide.description}
+                            </h2>
+                            <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                                {slide.title}
+                            </h1>
+                            <Link href={slide.url}>
+                                <button className="rounded-md bg-black text-white py-3 px-4 ">
+                                SHOP NOW
+                                </button>
+                            </Link>
+                        </div>
+                        {/* IMAGE CONTAINER */}
+                        <div className="h-1/2 xl:w-1/2 xl:h-full relative">
+                            <Image
+                                src={slide.img}
+                                alt=""
+                                fill
+                                sizes="100%"
+                                className="object-cover"
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
