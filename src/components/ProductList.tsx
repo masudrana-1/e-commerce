cb81fec50a340a5044ffe7f51fc7a87c6f80811c
masudrana-1 <masudrana58855@gmail.com>
2024-05-31 19:58:02 +0600

    create product list

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+const ProductList = () => {
+    return (
+        <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+            <Link href="/product" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+            
+            </Link>
+        </div>
+    );
+};
+
+export default ProductList;
