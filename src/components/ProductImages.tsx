dfc4bb9acbf3e58ccf47540b879669937a784af3
masudrana-1 <masudrana58855@gmail.com>
2024-06-07 20:36:14 +0600

    added image

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
 
 
 const ProductImages = () => {
     return (
         <div>
             <div className="h-[500px] relative">
-
+                <Image
+                    src="https://images.unsplash.com/photo-1583744999783-efe9dc5eac91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D"
+                    alt=""
+                    fill
+                    sizes="50vw"
+                    className="object-cover rounded-md"
+                />
             </div>
             <div className="flex justify-between gap-4 mt-8">
 
