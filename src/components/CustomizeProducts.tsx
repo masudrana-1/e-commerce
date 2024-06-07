038213ab49028f7cac258159c13c927fd0871c57
masudrana-1 <masudrana58855@gmail.com>
2024-06-07 21:11:12 +0600

    create color and size section

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -2,8 +2,18 @@
 
 const CustomizeProducts = () => {
     return (
-        <div>
-            Product
+        <div className="flex flex-col gap-6">
+            {/* color section  */}
+            <h4 className="font-medium">Choose a color</h4>
+            <ul className="flex items-center gap-3">
+
+            </ul>
+
+            {/* size section */}
+            <h4 className="font-medium">Choose a size</h4>
+            <ul className="flex items-center gap-3">
+
+            </ul>
         </div>
     );
 };
