4bab45c75a0f3d7836504150ff5f8c795cd64471
masudrana-1 <masudrana58855@gmail.com>
2024-06-07 20:33:48 +0600

    added style in image

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,7 +3,12 @@
 const ProductImages = () => {
     return (
         <div>
-            
+            <div className="h-[500px] relative">
+
+            </div>
+            <div className="flex justify-between gap-4 mt-8">
+
+            </div>
         </div>
     );
 };
