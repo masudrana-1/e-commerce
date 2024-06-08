5b1e2eb049ecbbe73f18138364f811bdc8f2b1f4
masudrana-1 <masudrana58855@gmail.com>
2024-06-08 10:37:43 +0600

    added color

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -6,7 +6,13 @@ const CustomizeProducts = () => {
             {/* color section  */}
             <h4 className="font-medium">Choose a color</h4>
             <ul className="flex items-center gap-3">
-
+                <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative bg-red-500">
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                </li>
+                <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative bg-blue-600" />
+                <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative bg-purple-500">
+                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                </li>
             </ul>
 
             {/* size section */}
