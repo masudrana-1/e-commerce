572bf559f9c4dabdfe0027c4db7784e3ac2807a1
masudrana-1 <masudrana58855@gmail.com>
2024-06-08 10:56:42 +0600

    create add to cart section

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+"use client"
+
+import { useState } from 'react';
 
 const Add = () => {
+
+    const [quantity, setQuantity] = useState(1);
+
+    // TEMPORARY
+    const stockNumber = 4;
+
+    // quantity function 
+    const handleQuantity = (type: "i" | "d") => {
+        if (type === "d" && quantity > 1) {
+            setQuantity((prev) => prev - 1);
+        }
+        if (type === "i" && quantity < stockNumber) {
+            setQuantity((prev) => prev + 1);
+        }
+    };
+
     return (
         <div className="flex flex-col gap-4">
             <h4 className="font-medium">Choose a Quantity</h4>
             <div className="flex justify-between">
-                <div>
-                    
+                <div className="flex items-center gap-4">
+                    <div className="bg-gray-200 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
+                        <button
+                            className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
+                            onClick={() => handleQuantity("d")}
+                        >
+                        -
+                        </button>
+                            {quantity}
+                        <button
+                        className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
+                        onClick={() => handleQuantity("i")}
+                        >
+                        +
+                        </button>
+                    </div>
+                    <div className="text-xs">
+                        Only <span className="text-orange-500">4 items</span>{" "}
+                        left!
+                        <br /> {"Don't"} miss it
+                    </div>
                 </div>
                 <button
                     className="w-36 text-sm rounded-3xl ring-1 ring-masud text-masud py-2 px-4 hover:bg-masud hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
