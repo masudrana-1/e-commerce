de924f3ab127699b8e40a5b0b1c4da60ee6b72c8
masudrana-1 <masudrana58855@gmail.com>
2024-06-08 10:44:59 +0600

    create add section and add to cart btn

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Add = () => {
     return (
-        <div>
-            Add
+        <div className="flex flex-col gap-4">
+            <h4 className="font-medium">Choose a Quantity</h4>
+            <div className="flex justify-between">
+                <div>
+                    
+                </div>
+                <button
+                    className="w-36 text-sm rounded-3xl ring-1 ring-masud text-masud py-2 px-4 hover:bg-masud hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
+                >
+                    Add to Cart
+                </button>
+            </div>
         </div>
     );
 };
