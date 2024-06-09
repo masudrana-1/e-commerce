9a7da753ff219b387960e0846f5a798671efa847
masudrana-1 <masudrana58855@gmail.com>
2024-06-09 20:26:07 +0600

    check and create access token

--- src/context/wixContext.tsx
+++ src/context/wixContext.tsx
@@ -2,26 +2,26 @@
 
 import { createClient, OAuthStrategy } from "@wix/sdk";
 import { products, collections } from "@wix/stores"
+import Cookies from "js-cookie";
 
-    const myWixClient = createClient({
-        modules: {
-            products,
-            // currentCart
+
+// check 1st time refresh token 
+const refreshToken = JSON.parse(Cookies.get("refreshToken") || "{}");
+
+const myWixClient = createClient({
+    modules: {
+        products,
+        // currentCart
+    },
+    auth: OAuthStrategy({
+
+        // wix client id 
+        clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+
+        // create token 
+        tokens: {
+            refreshToken,
+            accessToken: { value: "", expiresAt: 0 },
         },
-        auth: OAuthStrategy({
-
-            // wix client id 
-            clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
-
-            // create token 
-            tokens: {
-                accessToken: {
-                    value: '<ACCESS_TOKEN_VALUE>',
-                    expiresAt: <ACCESS_TOKEN_EXPIRY_DATE>
-                },
-                refreshToken: {
-                    value: '<REFRESH_TOKEN_VALUE>'
-                }
-            }
-        })
-    })
+    }),
+});
