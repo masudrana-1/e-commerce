152e07e92decc5b3c1e4d978a6b6986b2f49c5a1
masudrana-1 <masudrana58855@gmail.com>
2024-06-09 20:21:19 +0600

    install js cookie for refresh token

--- src/context/wixContext.tsx
+++ src/context/wixContext.tsx
@@ -1,4 +1,27 @@
 "use client"
 
 import { createClient, OAuthStrategy } from "@wix/sdk";
+import { products, collections } from "@wix/stores"
 
+    const myWixClient = createClient({
+        modules: {
+            products,
+            // currentCart
+        },
+        auth: OAuthStrategy({
+
+            // wix client id 
+            clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+
+            // create token 
+            tokens: {
+                accessToken: {
+                    value: '<ACCESS_TOKEN_VALUE>',
+                    expiresAt: <ACCESS_TOKEN_EXPIRY_DATE>
+                },
+                refreshToken: {
+                    value: '<REFRESH_TOKEN_VALUE>'
+                }
+            }
+        })
+    })
