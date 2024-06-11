f25297f1f37cf970f3febc3371a803be8c2bb37b
masudrana-1 <masudrana58855@gmail.com>
2024-06-11 11:30:28 +0600

    show feature items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,11 +40,10 @@ const HomePage = async() => {
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <Suspense
-          categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID}
-          limit={4}
-        >
-          <ProductList/>
+        <Suspense fallback={"loading"}>
+          <ProductList 
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}/>
         </Suspense>
       </div>
       <div className="mt-24">
@@ -55,7 +54,12 @@ const HomePage = async() => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-          <ProductList/>
+          <Suspense fallback={"loading"}>
+            <ProductList
+              categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+              limit={4}
+            />
+          </Suspense>
       </div>
     </div>
   )
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,45 +1,63 @@
+import { wixClientServer } from "@/lib/wixClientServer";
+import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
 
+// for new product
+const PRODUCT_PER_PAGE = 8;
 
-const ProductList = async ({categoryId, limit}: {categoryId: string; limit?: number;}) => {
+const ProductList = async ({ categoryId, limit }: { categoryId: string; limit?: number; }) => {
+    
+    const wixClient = await wixClientServer();
+        
+
+
+    const res = await wixClient.products.queryProducts().eq("collectionIds", categoryId).limit(limit || PRODUCT_PER_PAGE).find();
+
+
     return (
         <div
             className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap"
         >
-            <Link
-                href="/product"
-                className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-            >
-                <div
-                    className="relative w-full h-80"
+            {res.items.map((product: products.Product) => (
+                <Link
+                    href={"/" + product.slug}
+                    className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+                    key={product._id}
                 >
+                <div className="relative w-full h-80">
                     <Image
-                        src="https://images.unsplash.com/photo-1554568218-0f1715e72254?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D"
+                        src={product.media?.mainMedia?.image?.url || "/product.png"}
                         alt=""
                         fill
-                        sizes="25w"
+                        sizes="25vw"
                         className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
                     />
-                    <Image
-                        src="https://images.unsplash.com/photo-1583744946564-b52ac1c389c8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8dCUyMHNoaXJ0fGVufDB8fDB8fHww"
-                        alt=""
-                        fill
-                        sizes="25w"
-                        className="absolute object-cover rounded-md"
-                    />
+                    {product.media?.items && (
+                        <Image
+                            src={product.media?.items[1]?.image?.url || "/product.png"}
+                            alt=""
+                            fill
+                            sizes="25vw"
+                            className="absolute object-cover rounded-md"
+                        />
+                    )}
                 </div>
                 <div className="flex justify-between">
-                    <span className="font-medium">T-Shirt</span>
-                    <span className="font-semibold">$79</span>
+                    <span className="font-medium">{product.name}</span>
+                    <span className="font-semibold">${product.price?.price}</span>
                 </div>
-                <div className="text-sm text-gray-500">
-                    Description
-                </div>
+                {product.additionalInfoSections && (
+                    <div
+                        className="text-sm text-gray-500"
+                        
+                    ></div>
+                )}
                 <button className="rounded-2xl ring-1 ring-masud text-masud w-max py-2 px-4 text-xs hover:bg-masud hover:text-white">
                     Add to Cart
                 </button>
-            </Link>
+                </Link>
+            ))}
         </div>
     );
 };
