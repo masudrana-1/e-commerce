f33d76e4a57df0337b5092328542113a27c4b15e
masudrana-1 <masudrana58855@gmail.com>
2024-06-11 11:45:06 +0600

    fixed some bug

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,6 @@
 import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
+import DOMPurify from "isomorphic-dompurify";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -50,7 +51,13 @@ const ProductList = async ({ categoryId, limit }: { categoryId: string; limit?: 
                 {product.additionalInfoSections && (
                     <div
                         className="text-sm text-gray-500"
-                        
+                        dangerouslySetInnerHTML={{
+                            __html: DOMPurify.sanitize(
+                                product.additionalInfoSections.find(
+                                    (section: any) => section.title === "shortDesc"
+                                )?.description || ""
+                            )
+                        }}
                     ></div>
                 )}
                 <button className="rounded-2xl ring-1 ring-masud text-masud w-max py-2 px-4 text-xs hover:bg-masud hover:text-white">
