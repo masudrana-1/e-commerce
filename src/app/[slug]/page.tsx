76d0e94540d6fe4dd7fa4ca4e69e77ec5b8ab2cc
masudrana-1 <masudrana58855@gmail.com>
2024-06-12 21:18:15 +0600

    show data

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Add from '@/components/Add';
 import CustomizeProducts from '@/components/CustomizeProducts';
 import ProductImages from '@/components/ProductImages';
 import { wixClientServer } from '@/lib/wixClientServer';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 const SinglePage = async ({ params }: { params: { slug: string } }) => {
@@ -16,7 +17,7 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
         .find();
 
     if (!products.items[0]) {
-        // return notFound();
+        return notFound();
     }
 
     const product = products.items[0];
@@ -25,21 +26,21 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
         <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
             {/* img  */}
             <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-                <ProductImages/>
+                <ProductImages items={product.media?.items}/>
             </div>
 
             {/* text */}
             <div className="w-full lg:w-1/2 flex flex-col gap-6">
-                <h1 className="text-4xl font-medium">Product Name</h1>
-                <p className="text-gray-500">Product description</p>
+                <h1 className="text-4xl font-medium">{product.name}</h1>
+                <p className="text-gray-500">{product.description}</p>
                 <div className="h-[2px] bg-gray-100" />
                 {/* price section  */}
                 <div className="flex items-center gap-4">
                     <h3 className="text-xl text-gray-500 line-through">
-                        $45.00
+                        {product.price?.price}
                     </h3>
                     <h2 className="font-medium text-2xl">
-                        $35.00
+                        ${product.price?.discountedPrice}
                     </h2>
                 </div>
                 <div className="h-[2px] bg-gray-100" />
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,24 +3,24 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const images = [
-    {
-        _id: 1,
-        url: "https://images.unsplash.com/photo-1583744999783-efe9dc5eac91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
-    },
-    {
-        _id: 2,
-        url: "https://plus.unsplash.com/premium_photo-1670282392820-e3590c1c5c54?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
-    },
-    {
-        _id: 3,
-        url: "https://plus.unsplash.com/premium_photo-1687850859076-b8e74a1ac8fa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDl8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
-    },
-    {
-        _id: 4,
-        url: "https://images.unsplash.com/photo-1622445275649-b1922cc3e837?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
-    },
-];
+// const images = [
+//     {
+//         _id: 1,
+//         url: "https://images.unsplash.com/photo-1583744999783-efe9dc5eac91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+//     },
+//     {
+//         _id: 2,
+//         url: "https://plus.unsplash.com/premium_photo-1670282392820-e3590c1c5c54?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+//     },
+//     {
+//         _id: 3,
+//         url: "https://plus.unsplash.com/premium_photo-1687850859076-b8e74a1ac8fa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDl8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+//     },
+//     {
+//         _id: 4,
+//         url: "https://images.unsplash.com/photo-1622445275649-b1922cc3e837?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+//     },
+// ];
 
 
 
