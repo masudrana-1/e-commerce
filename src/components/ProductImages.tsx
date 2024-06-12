6cb2a468cd56625a156d0369151b5cba5e1191b8
masudrana-1 <masudrana58855@gmail.com>
2024-06-12 21:22:49 +0600

    show img

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -24,7 +24,7 @@ import { useState } from "react";
 
 
 
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
 
     const [index, setIndex] = useState(0);
 
@@ -32,7 +32,7 @@ const ProductImages = () => {
         <div>
             <div className="h-[500px] relative">
                 <Image
-                    src={images[index].url}
+                    src={items[index].image?.url}
                     alt=""
                     fill
                     sizes="50vw"
@@ -40,14 +40,14 @@ const ProductImages = () => {
                 />
             </div>
             <div className="flex justify-between gap-4 mt-8">
-                {images.map((img:any, i:number) => (
+                {items.map((item:any, i:number) => (
                     <div
                         className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-                        key={img._id}
+                        key={item._id}
                         onClick={() => setIndex(i)}
                     >
                         <Image
-                            src={img.url}
+                            src={item.image?.url}
                             alt=""
                             fill
                             sizes="30vw"
