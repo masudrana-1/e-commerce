2bde0453c262a74333e9449900a7494cf2ea390d
masudrana-1 <masudrana58855@gmail.com>
2024-06-13 10:15:33 +0600

    create selected state and function

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -1,34 +1,64 @@
 "use client"
 
 import { products } from "@wix/stores";
+import { useState } from "react";
 
 
 const CustomizeProducts = ({
-    productId,
-    variants,
-    productOptions,
-}: {
-    productId: string;
-    variants: products.Variant[];
-    productOptions: products.ProductOption[];
-}) => {
+        productId,
+        variants,
+        productOptions,
+    }: {
+        productId: string;
+        variants: products.Variant[];
+        productOptions: products.ProductOption[];
+    }) => {
+    
+    // selected state 
+    const [selectedOptions, setSelectedOptions] = useState<{ [key: string]: string; }>({});
+    
+
+    // selected function 
+    const handleOptionSelect = (optionType: string, choice: string) => {
+        setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
+    };
+    
+    
     return (
         <div className="flex flex-col gap-6">
-            {productOptions?.map((option) => (
+            {productOptions.map((option) => (
                 <div className="flex flex-col gap-4" key={option.name}>
-                    <h4 className="font-medium">Choose a {option?.name}</h4>
+                    <h4 className="font-medium">Choose a {option.name}</h4>
                     <ul className="flex items-center gap-3">
-                        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative bg-red-500">
-                            <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-                        </li>
-                        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative bg-blue-600" />
-                        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative bg-purple-500">
-                            <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-                        </li>
+                        {option.choices?.map((choice) => {
+                                [option.name!]: choice.description!,
+                            
+
+                            const selected =
+                                selectedOptions[option.name!] === choice.description;
+
+                            const clickHandler = disabled
+                                ? undefined
+                                : () => handleOptionSelect(option.name!, choice.description!);
+
+                            return option.name === "Color" ? (
+                                <li
+                            className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative"
+                            
+                                >
+                                </li>
+                            ) : (
+                                <li
+                                    className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm"
+                                    
+                                >
+                                    {/* {choice.description} */}
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
-                ))
-            }
+            ))}
 
             {/* size section */}
             {/* <h4 className="font-medium">Choose a size</h4>
