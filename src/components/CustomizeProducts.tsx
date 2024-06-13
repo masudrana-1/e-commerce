f83798eee023127c91cbe07910394ac46173b5c2
masudrana-1 <masudrana58855@gmail.com>
2024-06-13 10:25:10 +0600

    check stocks and show data

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -23,6 +23,23 @@ const CustomizeProducts = ({
         setSelectedOptions((prev) => ({ ...prev, [optionType]: choice }));
     };
     
+
+    // check stocks 
+    const isVariantInStock = (choices: { [key: string]: string }) => {
+        return variants.some((variant) => {
+            const variantChoices = variant.choices;
+            if (!variantChoices) return false;
+
+            return (
+                Object.entries(choices).every(
+                    ([key, value]) => variantChoices[key] === value
+                ) &&
+                variant.stock?.inStock &&
+                variant.stock?.quantity &&
+                variant.stock?.quantity > 0
+            );
+        });
+    };
     
     return (
         <div className="flex flex-col gap-6">
@@ -31,8 +48,10 @@ const CustomizeProducts = ({
                     <h4 className="font-medium">Choose a {option.name}</h4>
                     <ul className="flex items-center gap-3">
                         {option.choices?.map((choice) => {
+                            const disabled = !isVariantInStock({
+                                ...selectedOptions,
                                 [option.name!]: choice.description!,
-                            
+                            });
 
                             const selected =
                                 selectedOptions[option.name!] === choice.description;
@@ -43,16 +62,39 @@ const CustomizeProducts = ({
 
                             return option.name === "Color" ? (
                                 <li
-                            className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative"
-                            
+                                    className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative"
+                                    style={{
+                                        backgroundColor: choice.value,
+                                        cursor: disabled ? "not-allowed" : "pointer",
+                                    }}
+                                    onClick={clickHandler}
+                                    key={choice.description}
                                 >
+                                    {selected && (
+                                        <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                                    )}
+                                    {disabled && (
+                                        <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                                    )}
                                 </li>
                             ) : (
                                 <li
                                     className="ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm"
-                                    
+                                    style={{
+                                        cursor: disabled ? "not-allowed" : "pointer",
+                                        backgroundColor: selected
+                                        ? "#f35c7a"
+                                        : disabled
+                                        ? "#FBCFE8"
+                                        : "white",
+                                        color: selected || disabled ? "white" : "#f35c7a",
+                                        boxShadow: disabled ? "none" : "",
+                                        
+                                    }}
+                                    key={choice.description}
+                                    onClick={clickHandler}
                                 >
-                                    {/* {choice.description} */}
+                                    {choice.description}
                                 </li>
                             );
                         })}
