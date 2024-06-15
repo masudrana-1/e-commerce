6701357e9e7e0cee76136f59ac031bde09f89dc6
masudrana-1 <masudrana58855@gmail.com>
2024-06-15 20:04:45 +0600

    fixed some bug

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -71,7 +71,7 @@ const Add = ({
                 </div>
                 <button
                     
-                    className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
+                    className="w-36 text-sm rounded-3xl ring-1 ring-masud text-masud py-2 px-4 hover:bg-masud hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
                 >
                 Add to Cart
                 </button>
