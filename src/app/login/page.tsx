5797cfa425bc5f53fbe922fd1a5ed1a34f6fcf91
masudrana-1 <masudrana58855@gmail.com>
2024-07-02 20:19:45 +0600

    create all state

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,30 @@
 "use client"
 
+import { useState } from "react";
+
 
 enum MODE {
     LOGIN = "LOGIN",
     REGISTER = "REGISTER",
     RESET_PASSWORD = "RESET_PASSWORD",
     EMAIL_VERIFICATION = "EMAIL_VERIFICATION",
-}
+};
 
 
 const LoginPage = () => {
+
+
+    // all states
+    const [mode, setMode] = useState(MODE.LOGIN);
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailCode, setEmailCode] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [message, setMessage] = useState("");
+
     return (
         <div>
             loginPage
