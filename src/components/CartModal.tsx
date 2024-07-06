af31d373b33cf4fa91aaead617dd652bb53af7cf
masudrana-1 <masudrana58855@gmail.com>
2024-07-06 10:15:43 +0600

    create cart

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,8 +1,16 @@
+"use client"
+
+
+import { useWixClient } from "@/hooks/useWixClient";
 
 
 const CartModal = () => {
+
+    // wix state 
+    const wixClient = useWixClient();
+
     return (
-        <div>
+        <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
             
         </div>
     );
